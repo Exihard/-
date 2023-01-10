256c8c82a5ebdd3040e024bc8466ea97d2abfb5c
Korehov_Roman_Nikolaevich <rkorekhov@yandex.ru>
2023-01-10 22:01:20 +0300

    Блок сортировки и вывода элементов массива

--- Program.cs
+++ Program.cs
@@ -10,3 +10,29 @@ for (int i = 0; i < size; i++)
     string element = Convert.ToString(Console.ReadLine());
     array[i] = element;
 }
+
+Console.WriteLine("Введите разделитель между элементами!");
+string divider = Convert.ToString(Console.ReadLine());
+
+void Less_3_Array (string [] array){
+      string[] NewArray = new string[array.Length];
+      for (int j=0; j < array.Length; j++)
+      {      
+         if(array[j].Length<=3)
+         {
+         NewArray[j]=array[j];
+         Console.ForegroundColor = ConsoleColor.Green;
+         Console.Write($"{NewArray[j]}");
+         Console.ResetColor();
+            if(j < array.Length-1)
+            {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write($"{divider}");
+            Console.ResetColor();
+            }
+         }
+      }
+        Console.WriteLine();
+}
+
+Less_3_Array(array);
