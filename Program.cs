e4308cdf1b4b05a6804d62e13aac38a5437c5395
Korehov_Roman_Nikolaevich <rkorekhov@yandex.ru>
2023-01-10 21:56:45 +0300

    Блок заполнения массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющихся массива строк формирует массив из строк, длина которых меньше либо равна 3 символам.
+
+Console.WriteLine("Сколько элементов будет в массиве?");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] array = new string[size];
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"Введите {i+1}-й элемент: ");
+    string element = Convert.ToString(Console.ReadLine());
+    array[i] = element;
+}
